0f36b9efda2c8cba8c95a9605e3bf42773748d08
Ashkar Yoosuf <ashkar.15@cse.mrt.ac.lk>
2019-07-08 21:11:39 +0530

    edits

--- mean_filterGPU.cu
+++ mean_filterGPU.cu
@@ -6,20 +6,16 @@
 #include <time.h>
 #define TILE_SIZE 4 
 
-__global__ void meanFilterSharedKernel(float *inputImageKernel, float *outputImagekernel, int imageWidth, int imageHeight)
+__global__ void meanFilterSharedKernel(float *inputImageKernel, float *outputImagekernel, int dim)
 {
-	//Set the row and col value for each thread.
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     
-    //initialize shared memory
     __shared__ float sharedmem[(TILE_SIZE+2)][(TILE_SIZE+2)];
     
-	//Take some values.
 	bool is_x_left = (threadIdx.x == 0), is_x_right = (threadIdx.x == TILE_SIZE-1);
     bool is_y_top = (threadIdx.y == 0), is_y_bottom = (threadIdx.y == TILE_SIZE-1);
 
-	//Initialize with zero
 	if(is_x_left)
 		sharedmem[threadIdx.x][threadIdx.y+1] = 0;
 	else if(is_x_right)
@@ -39,31 +35,29 @@ __global__ void meanFilterSharedKernel(float *inputImageKernel, float *outputIma
 			sharedmem[threadIdx.x][threadIdx.y+2] = 0;
 	}
 
-	//Setup pixel values
-	sharedmem[threadIdx.x+1][threadIdx.y+1] = inputImageKernel[row*imageWidth+col];
-	//Check for boundry conditions.
+	sharedmem[threadIdx.x+1][threadIdx.y+1] = inputImageKernel[row*dim+col];
+
 	if(is_x_left && (col>0))
-		sharedmem[threadIdx.x][threadIdx.y+1] = inputImageKernel[row*imageWidth+(col-1)];
-	else if(is_x_right && (col<imageWidth-1))
-		sharedmem[threadIdx.x + 2][threadIdx.y+1]= inputImageKernel[row*imageWidth+(col+1)];
+		sharedmem[threadIdx.x][threadIdx.y+1] = inputImageKernel[row*dim+(col-1)];
+	else if(is_x_right && (col<dim-1))
+		sharedmem[threadIdx.x + 2][threadIdx.y+1]= inputImageKernel[row*dim+(col+1)];
 	if (is_y_top && (row>0)){
-		sharedmem[threadIdx.x+1][threadIdx.y] = inputImageKernel[(row-1)*imageWidth+col];
+		sharedmem[threadIdx.x+1][threadIdx.y] = inputImageKernel[(row-1)*dim+col];
 		if(is_x_left)
-			sharedmem[threadIdx.x][threadIdx.y] = inputImageKernel[(row-1)*imageWidth+(col-1)];
+			sharedmem[threadIdx.x][threadIdx.y] = inputImageKernel[(row-1)*dim+(col-1)];
 		else if(is_x_right )
-			sharedmem[threadIdx.x+2][threadIdx.y] = inputImageKernel[(row-1)*imageWidth+(col+1)];
+			sharedmem[threadIdx.x+2][threadIdx.y] = inputImageKernel[(row-1)*dim+(col+1)];
 	}
-	else if (is_y_bottom && (row<imageHeight-1)){
-		sharedmem[threadIdx.x+1][threadIdx.y+2] = inputImageKernel[(row+1)*imageWidth + col];
+	else if (is_y_bottom && (row<dim-1)){
+		sharedmem[threadIdx.x+1][threadIdx.y+2] = inputImageKernel[(row+1)*dim + col];
 		if(is_x_right)
-			sharedmem[threadIdx.x+2][threadIdx.y+2] = inputImageKernel[(row+1)*imageWidth+(col+1)];
+			sharedmem[threadIdx.x+2][threadIdx.y+2] = inputImageKernel[(row+1)*dim+(col+1)];
 		else if(is_x_left)
-			sharedmem[threadIdx.x][threadIdx.y+2] = inputImageKernel[(row+1)*imageWidth+(col-1)];
+			sharedmem[threadIdx.x][threadIdx.y+2] = inputImageKernel[(row+1)*dim+(col-1)];
 	}
 
-	__syncthreads();   //Wait for all threads to be done.
+	__syncthreads();
 
-	//Setup the filter.
     float filterVector[9] = {sharedmem[threadIdx.x][threadIdx.y], sharedmem[threadIdx.x+1][threadIdx.y],
                     sharedmem[threadIdx.x+2][threadIdx.y], sharedmem[threadIdx.x][threadIdx.y+1],
                     sharedmem[threadIdx.x+1][threadIdx.y+1], sharedmem[threadIdx.x+2][threadIdx.y+1],
@@ -77,23 +71,20 @@ __global__ void meanFilterSharedKernel(float *inputImageKernel, float *outputIma
     for (int i = 0; i < 9; i++) {
         element += filterVector[i];
     }
-	outputImagekernel[row*imageWidth+col] = element/9.0;   //Set the output image values.
+	outputImagekernel[row*dim+col] = element/9.0;
 	}
 }
 
 bool meanFilterGPU(float* image, float* outputImage){
-	//Cuda error and image values.
 	cudaEvent_t start,stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 	cudaEventRecord(start);
 	cudaError_t status;
-	int width = 1280;
-	int height = 1280;
+	int dim = 1280;
 
-    int size =  width * height * sizeof(float);
+    int size =  dim * dim * sizeof(float);
     
-	//initialize images.
 	float *deviceinputimage;
     cudaMalloc((void**) &deviceinputimage, size);
     
@@ -112,39 +103,33 @@ bool meanFilterGPU(float* image, float* outputImage){
     float *deviceOutputImage;
     cudaMalloc((void**) &deviceOutputImage, size);
     
-	//take block and grids.
 	dim3 dimBlock(TILE_SIZE, TILE_SIZE);
-	dim3 dimGrid((int)ceil((float) width / (float)TILE_SIZE), (int)ceil((float) height / (float)TILE_SIZE));
+	dim3 dimGrid((int)ceil((float) dim / (float)TILE_SIZE), (int)ceil((float) dim / (float)TILE_SIZE));
     
     clock_t start_filter = clock();
-    meanFilterSharedKernel<<<dimGrid, dimBlock>>>(deviceinputimage, deviceOutputImage, width, height);
+    meanFilterSharedKernel<<<dimGrid, dimBlock>>>(deviceinputimage, deviceOutputImage, dim);
     clock_t end_filter = clock();
 
     double time_filter = (double)(end_filter-start_filter)/CLOCKS_PER_SEC;
     
-    printf("Image size : %d Window size : %d GPU Filter Time: %f\n", width, 3 , time_filter);
+    printf("Image size : %d Window size : %d GPU Filter Time: %f\n", dim, 3 , time_filter);
 	
-
-// save output image to host.
 	cudaMemcpy(outputImage, deviceOutputImage, size, cudaMemcpyDeviceToHost);
 	status = cudaGetLastError();              
 	
 
 
 if (status != cudaSuccess) {                     
-		// std::cout << "Kernel failed for cudaMemcpy cudaMemcpyDeviceToHost: " << cudaGetErrorString(status) << std::endl;
 		cudaFree(deviceinputimage);
 		cudaFree(deviceOutputImage);
 		return false;
 	}
-	//Free the memory
 	cudaFree(deviceinputimage);
 	cudaFree(deviceOutputImage);
 	cudaEventRecord(stop);
 	cudaEventSynchronize(stop);
 	float time = 0;
 	cudaEventElapsedTime(&time,start,stop);
-	// printf("time %f\n", time); 
 	return true;
 }
 
@@ -175,9 +160,6 @@ int main()
         img[i] = strtof(line, &endptr);
         i++;
     }
-
-	// float tgpushared; //timing variables.
-	// clock_t start, end;
 
 	meanFilterGPU(img, filtered_img); //GPU call for median Filtering with shared Kernel.
     
