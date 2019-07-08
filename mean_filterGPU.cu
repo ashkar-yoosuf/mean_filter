a01b94d1479426bfbab90b3d0af32d9254cb3443
Ashkar Yoosuf <ashkar.15@cse.mrt.ac.lk>
2019-07-09 00:30:19 +0530

    readable

--- mean_filterGPU.cu
+++ mean_filterGPU.cu
@@ -6,63 +6,63 @@
 #include <time.h>
 #define TILE_SIZE 4 
 
-__global__ void meanFilterSharedKernel(float *inputImageKernel, float *outputImagekernel, int dim)
+__global__ void meanFilter(float *deviceinputimage, float *deviceOutputImage, int dim)
 {
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     
-    __shared__ float sharedmem[(TILE_SIZE+2)][(TILE_SIZE+2)];
+    __shared__ float mem_shared[(TILE_SIZE+2)][(TILE_SIZE+2)];
     
 	bool is_x_left = (threadIdx.x == 0), is_x_right = (threadIdx.x == TILE_SIZE-1);
     bool is_y_top = (threadIdx.y == 0), is_y_bottom = (threadIdx.y == TILE_SIZE-1);
 
 	if(is_x_left)
-		sharedmem[threadIdx.x][threadIdx.y+1] = 0;
+		mem_shared[threadIdx.x][threadIdx.y+1] = 0;
 	else if(is_x_right)
-		sharedmem[threadIdx.x + 2][threadIdx.y+1]=0;
+		mem_shared[threadIdx.x + 2][threadIdx.y+1]=0;
 	if (is_y_top){
-		sharedmem[threadIdx.x+1][threadIdx.y] = 0;
+		mem_shared[threadIdx.x+1][threadIdx.y] = 0;
 		if(is_x_left)
-			sharedmem[threadIdx.x][threadIdx.y] = 0;
+			mem_shared[threadIdx.x][threadIdx.y] = 0;
 		else if(is_x_right)
-			sharedmem[threadIdx.x+2][threadIdx.y] = 0;
+			mem_shared[threadIdx.x+2][threadIdx.y] = 0;
 	}
 	else if (is_y_bottom){
-		sharedmem[threadIdx.x+1][threadIdx.y+2] = 0;
+		mem_shared[threadIdx.x+1][threadIdx.y+2] = 0;
 		if(is_x_right)
-			sharedmem[threadIdx.x+2][threadIdx.y+2] = 0;
+			mem_shared[threadIdx.x+2][threadIdx.y+2] = 0;
 		else if(is_x_left)
-			sharedmem[threadIdx.x][threadIdx.y+2] = 0;
+			mem_shared[threadIdx.x][threadIdx.y+2] = 0;
 	}
 
-	sharedmem[threadIdx.x+1][threadIdx.y+1] = inputImageKernel[row*dim+col];
+	mem_shared[threadIdx.x+1][threadIdx.y+1] = deviceinputimage[row*dim+col];
 
 	if(is_x_left && (col>0))
-		sharedmem[threadIdx.x][threadIdx.y+1] = inputImageKernel[row*dim+(col-1)];
+		mem_shared[threadIdx.x][threadIdx.y+1] = deviceinputimage[row*dim+(col-1)];
 	else if(is_x_right && (col<dim-1))
-		sharedmem[threadIdx.x + 2][threadIdx.y+1]= inputImageKernel[row*dim+(col+1)];
+		mem_shared[threadIdx.x + 2][threadIdx.y+1]= deviceinputimage[row*dim+(col+1)];
 	if (is_y_top && (row>0)){
-		sharedmem[threadIdx.x+1][threadIdx.y] = inputImageKernel[(row-1)*dim+col];
+		mem_shared[threadIdx.x+1][threadIdx.y] = deviceinputimage[(row-1)*dim+col];
 		if(is_x_left)
-			sharedmem[threadIdx.x][threadIdx.y] = inputImageKernel[(row-1)*dim+(col-1)];
+			mem_shared[threadIdx.x][threadIdx.y] = deviceinputimage[(row-1)*dim+(col-1)];
 		else if(is_x_right )
-			sharedmem[threadIdx.x+2][threadIdx.y] = inputImageKernel[(row-1)*dim+(col+1)];
+			mem_shared[threadIdx.x+2][threadIdx.y] = deviceinputimage[(row-1)*dim+(col+1)];
 	}
 	else if (is_y_bottom && (row<dim-1)){
-		sharedmem[threadIdx.x+1][threadIdx.y+2] = inputImageKernel[(row+1)*dim + col];
+		mem_shared[threadIdx.x+1][threadIdx.y+2] = deviceinputimage[(row+1)*dim + col];
 		if(is_x_right)
-			sharedmem[threadIdx.x+2][threadIdx.y+2] = inputImageKernel[(row+1)*dim+(col+1)];
+			mem_shared[threadIdx.x+2][threadIdx.y+2] = deviceinputimage[(row+1)*dim+(col+1)];
 		else if(is_x_left)
-			sharedmem[threadIdx.x][threadIdx.y+2] = inputImageKernel[(row+1)*dim+(col-1)];
+			mem_shared[threadIdx.x][threadIdx.y+2] = deviceinputimage[(row+1)*dim+(col-1)];
 	}
 
 	__syncthreads();
 
-    float filterVector[9] = {sharedmem[threadIdx.x][threadIdx.y], sharedmem[threadIdx.x+1][threadIdx.y],
-                    sharedmem[threadIdx.x+2][threadIdx.y], sharedmem[threadIdx.x][threadIdx.y+1],
-                    sharedmem[threadIdx.x+1][threadIdx.y+1], sharedmem[threadIdx.x+2][threadIdx.y+1],
-                    sharedmem[threadIdx.x] [threadIdx.y+2], sharedmem[threadIdx.x+1][threadIdx.y+2],
-                    sharedmem[threadIdx.x+2][threadIdx.y+2]};
+    float filterVector[9] = {mem_shared[threadIdx.x][threadIdx.y], mem_shared[threadIdx.x+1][threadIdx.y],
+							mem_shared[threadIdx.x+2][threadIdx.y], mem_shared[threadIdx.x][threadIdx.y+1],
+							mem_shared[threadIdx.x+1][threadIdx.y+1], mem_shared[threadIdx.x+2][threadIdx.y+1],
+							mem_shared[threadIdx.x] [threadIdx.y+2], mem_shared[threadIdx.x+1][threadIdx.y+2],
+							mem_shared[threadIdx.x+2][threadIdx.y+2]};
 
 	
 	{
@@ -71,7 +71,7 @@ __global__ void meanFilterSharedKernel(float *inputImageKernel, float *outputIma
     for (int i = 0; i < 9; i++) {
         element += filterVector[i];
     }
-	outputImagekernel[row*dim+col] = element/9.0;
+	deviceOutputImage[row*dim+col] = element/9.0;
 	}
 }
 
@@ -85,47 +85,47 @@ bool meanFilterGPU(float* image, float* outputImage){
 
     int size =  dim * dim * sizeof(float);
     
-	float *deviceinputimage;
-    cudaMalloc((void**) &deviceinputimage, size);
+	float *inputimage_D;
+    cudaMalloc((void**) &inputimage_D, size);
     
 	status = cudaGetLastError();              
 	if (status != cudaSuccess) {                     
 		return false;
     }
     
-	cudaMemcpy(deviceinputimage, image, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(inputimage_D, image, size, cudaMemcpyHostToDevice);
 	status = cudaGetLastError();              
 	if (status != cudaSuccess) {                     
-		cudaFree(deviceinputimage);
+		cudaFree(inputimage_D);
 		return false;
 	}
     
-    float *deviceOutputImage;
-    cudaMalloc((void**) &deviceOutputImage, size);
+    float *outputImage_D;
+    cudaMalloc((void**) &outputImage_D, size);
     
 	dim3 dimBlock(TILE_SIZE, TILE_SIZE);
 	dim3 dimGrid((int)ceil((float) dim / (float)TILE_SIZE), (int)ceil((float) dim / (float)TILE_SIZE));
     
     clock_t start_filter = clock();
-    meanFilterSharedKernel<<<dimGrid, dimBlock>>>(deviceinputimage, deviceOutputImage, dim);
+    meanFilter<<<dimGrid, dimBlock>>>(inputimage_D, outputImage_D, dim);
     clock_t end_filter = clock();
 
     double time_filter = (double)(end_filter-start_filter)/CLOCKS_PER_SEC;
     
     printf("Image size : %d Window size : %d GPU Filter Time: %f\n", dim, 3 , time_filter);
 	
-	cudaMemcpy(outputImage, deviceOutputImage, size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(outputImage, outputImage_D, size, cudaMemcpyDeviceToHost);
 	status = cudaGetLastError();              
 	
 
 
 if (status != cudaSuccess) {                     
-		cudaFree(deviceinputimage);
-		cudaFree(deviceOutputImage);
+		cudaFree(inputimage_D);
+		cudaFree(outputImage_D);
 		return false;
 	}
-	cudaFree(deviceinputimage);
-	cudaFree(deviceOutputImage);
+	cudaFree(inputimage_D);
+	cudaFree(outputImage_D);
 	cudaEventRecord(stop);
 	cudaEventSynchronize(stop);
 	float time = 0;
